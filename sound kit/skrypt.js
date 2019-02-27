bda93463869f432c59fca8bdf638922ffd3e2de3
MikoTheAble <fuksior99775533@gmail.com>
2019-02-27 20:14:09 +0100

    1. danie dźwięku na spację było głupim pomysłem więc to zmieniłem. 2 podmienianie kolorów na switchu 3. dodanie pozostałych 3 kanałów nagrywania. Rozwiązanie jest mało elegandzkie bo jest dużo zdublowanego kodu ale działa przynajmniej tak jak powinno.

--- sound kit/skrypt.js
+++ sound kit/skrypt.js
@@ -35,8 +35,14 @@ document.querySelector('#nagrywanie').addEventListener('click', function (e){
   const kanal3 = []
   const kanal4 = []
   
-  let nagrywa = false
-  let startNagrywania = 0
+  let nagrywa = false;
+  let nagrywa2 = false;
+  let nagrywa3 = false;
+  let nagrywa4 = false;
+  let startNagrywania = 0;
+  let startNagrywania2 = 0;
+  let startNagrywania3 = 0;
+  let startNagrywania4 = 0;
   
   const dzwieki = {
       //małe litery
@@ -48,7 +54,8 @@ document.querySelector('#nagrywanie').addEventListener('click', function (e){
       105: "ride",
       111: "snare",
       112: "tink",
-      32: "tom",
+      110: "tom",
+      118: "tom",
   
       81: "boom",
       87: "clap",
@@ -57,7 +64,9 @@ document.querySelector('#nagrywanie').addEventListener('click', function (e){
       85: "openhat",
       73: "ride",
       79: "snare",
-      80: "tink"
+      80: "tink",
+      78: "tom",
+      86: "tom",
   }
   
   function appStart() {
@@ -65,12 +74,39 @@ document.querySelector('#nagrywanie').addEventListener('click', function (e){
   
       document.querySelector('#nagrywaj').addEventListener('click', nagrywaj)
       document.querySelector('#graj').addEventListener('click', odtwarzaj)
+
+      document.querySelector('#nagrywaj2').addEventListener('click', nagrywaj2)
+      document.querySelector('#graj2').addEventListener('click', odtwarzaj2)
+
+      document.querySelector('#nagrywaj3').addEventListener('click', nagrywaj3)
+      document.querySelector('#graj3').addEventListener('click', odtwarzaj3)
+
+      document.querySelector('#nagrywaj4').addEventListener('click', nagrywaj4)
+      document.querySelector('#graj4').addEventListener('click', odtwarzaj4)
   }
     
     function nagrywaj (e) {
       startNagrywania = Date.now()
       nagrywa = !nagrywa
       e.target.innerHTML = nagrywa ? 'Stop' : 'Nagrywaj' 
+    }
+
+    function nagrywaj2 (e) {
+      startNagrywania2 = Date.now()
+      nagrywa2 = !nagrywa2
+      e.target.innerHTML = nagrywa2 ? 'Stop' : 'Nagrywaj' 
+    }
+
+    function nagrywaj3 (e) {
+      startNagrywania3 = Date.now()
+      nagrywa3 = !nagrywa3
+      e.target.innerHTML = nagrywa3 ? 'Stop' : 'Nagrywaj' 
+    }
+
+    function nagrywaj4 (e) {
+      startNagrywania4 = Date.now()
+      nagrywa4 = !nagrywa4
+      e.target.innerHTML = nagrywa4 ? 'Stop' : 'Nagrywaj' 
     }
     
     function odtwarzaj () {
@@ -81,11 +117,36 @@ document.querySelector('#nagrywanie').addEventListener('click', function (e){
       })
     }
 
+    function odtwarzaj2 () {
+      kanal2.forEach(sound => {
+        setTimeout(
+          () => graj(sound.name), sound.time
+        )
+      })
+    }
+
+    function odtwarzaj3 () {
+      kanal2.forEach(sound => {
+        setTimeout(
+          () => graj(sound.name), sound.time
+        )
+      })
+    }
+
+    function odtwarzaj4 () {
+      kanal2.forEach(sound => {
+        setTimeout(
+          () => graj(sound.name), sound.time
+        )
+      })
+    }
+
  /**
- * @param {*} a
+ * @param {*} a       
  */
+
     function grajDzwiek (a) {
-      const nazwaDzwieku = dzwieki[a.keyCode]
+      const nazwaDzwieku = dzwieki[a.keyCode];
       
       graj(nazwaDzwieku);
     
@@ -97,7 +158,84 @@ document.querySelector('#nagrywanie').addEventListener('click', function (e){
           }
         )
       }
+
+      if(nagrywa2) {
+        kanal2.push(
+          {
+            name: nazwaDzwieku,
+            time: Date.now() - startNagrywania2
+          }
+        )
+      }
+
+      if(nagrywa3) {
+        kanal3.push(
+          {
+            name: nazwaDzwieku,
+            time: Date.now() - startNagrywania3
+          }
+        )
+      }
+
+      if(nagrywa4) {
+        kanal4.push(
+          {
+            name: nazwaDzwieku,
+            time: Date.now() - startNagrywania4
+          }
+        )
+      }
+
+      switch(nazwaDzwieku){      //Podmienia kolor granych dźwięków
+        case "boom": 
+        kq.style.backgroundColor = 'silver'
+        setTimeout(function(){ kq.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "clap": 
+        kw.style.backgroundColor = 'silver'
+        setTimeout(function(){ kw.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "hihat": 
+        ke.style.backgroundColor = 'silver'
+        setTimeout(function(){ ke.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "kick": 
+        kr.style.backgroundColor = 'silver'
+        setTimeout(function(){ kr.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "openhat": 
+        ku.style.backgroundColor = 'silver'
+        setTimeout(function(){ ku.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "ride": 
+        ki.style.backgroundColor = 'silver'
+        setTimeout(function(){ ki.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "snare": 
+        ko.style.backgroundColor = 'silver'
+        setTimeout(function(){ ko.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "tink": 
+        kp.style.backgroundColor = 'silver'
+        setTimeout(function(){ kp.style.backgroundColor = 'azure'; }, 200);
+        break;
+
+        case "tom": 
+        space.style.backgroundColor = 'silver'
+        setTimeout(function(){ space.style.backgroundColor = 'azure'; }, 200);
+        break;
+      }
+
+
     }
+
     
   
   function graj(nazwaDzwieku) {
@@ -105,3 +243,4 @@ document.querySelector('#nagrywanie').addEventListener('click', function (e){
     audioDOM.currentTime = 0;
     audioDOM.play();
   }
+
