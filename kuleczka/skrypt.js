e445762a89871a8a00ef740d1de8509727c3e024
MikoTheAble <43997963+MikoTheAble@users.noreply.github.com>
2019-03-19 00:03:13 +0100

    więcej commitów na moim drugim repo "testfull"

--- kuleczka/skrypt.js
+++ kuleczka/skrypt.js
@@ -1,40 +1,46 @@
-window.addEventListener("deviceorientation", handleOrientation, true);
+let kuleczka   = document.querySelector('.kuleczka');
+let dziura   = document.querySelector('.dziura');
+let boisko = document.querySelector('.boisko');
+
+let czasStart = Date.now();
+
+let maxX = boisko.clientWidth  - kuleczka.clientWidth;
+let maxY = boisko.clientHeight - kuleczka.clientHeight;
 
 function handleOrientation(event) {
-    let absolute = event.absolute;
-    let alpha    = event.alpha;
-    let beta     = event.beta;
-    let gamma    = event.gamma;
-  }
+  let x = event.beta; 
+  let y = event.gamma; 
 
-let kuleczka = document.querySelector('.kuleczka');
-let stol = document.querySelector('.stol');
-let output = document.querySelector('.output');
-
-let maxX = stol.clientWidth - kuleczka.clientWidth;
-let maxY = stol.clientHeight - kuleczka.clientHeight;
-
-function handleOrientation(event) {
-  let x = event.beta;  // In degree in the range [-180,180]
-  let y = event.gamma; // In degree in the range [-90,90]
-
-  output.innerHTML  = "beta : " + x + "\n";
-  output.innerHTML += "gamma: " + y + "\n";
-
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
+  if (x >  90) { x =  90};  //ma zapobiec sytuacji kiedy telefon musi być do góry nogami
   if (x < -90) { x = -90};
 
-  // To make computation easier we shift the range of 
-  // x and y to [0,180]
   x += 90;
   y += 90;
 
-  // 10 is half the size of the kuleczka
-  // It center the positioning point to the center of the kuleczka
-  kuleczka.style.top  = (maxX*x/180 - 10) + "px";
-  kuleczka.style.left = (maxY*y/180 - 10) + "px";
-}
+  kuleczka.style.top  = (maxX*x/90 - 10) + "px";
+  kuleczka.style.left = (maxY*y/90 - 10) + "px";
+  
+   if(kuleczka.style.top < 10) {       //te ify mają blokować kuleczke na ściankach
+        kuleczka.style.top = 10;
+    }
+    if(kuleczka.style.bottom < 10) {
+        kuleczka.style.bottom = 10;
+    }
+    if(kuleczka.style.right < 10) {
+        kuleczka.style.right = 10;
+    }
+    if(kuleczka.style.left < 10) {
+        kuleczka.style.left = 10;
+    }
 
+    let kuleczkaPosition = kuleczka.getBoundingClientRect();           // pobiera pozycje z elementu kuleczka
+    let dziuraPosition = dziura.getBoundingClientRect();              // pobiera położenie elementu dziura
+  
+      //Zakończenie
+    if((kuleczkaPosition.top <= dziuraPosition.top) && (kuleczkaPosition.bottom >= dziuraPosition.bottom) && (kuleczkaPosition.left >= dziuraPosition.left) &&
+    (kuleczkaPosition.right <= dziuraPosition.right)) {
+        let czas = Date.now() - czasStart; //podaje czas
+        alert("Wygrana! \n Twój czas : " + czas);
+    }
+}
 window.addEventListener('deviceorientation', handleOrientation);
